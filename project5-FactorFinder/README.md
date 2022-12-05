24c1b1789a196b6c90ae1044b7afa80d1ef7e267
Jules Ezeh <julesezeh23@gmail.com>
2022-12-05 15:15:52 +0100

    Function to find factors completed

---
+++ project5-FactorFinder/README.md
@@ -0,0 +1,10 @@
+A number’s factors are any two other numbers
+that, when multiplied with each other,
+produce the number. For example, 2 × 13 =
+26, so 2 and 13 are factors of 26. Also, 1 × 26 =
+26, so 1 and 26 are also factors of 26. Therefore, we
+say that 26 has four factors: 1, 2, 13, and 26.
+If a number only has two factors (1 and itself), we call that a prime
+number. Otherwise, we call it a composite number. Use the factor finder to
+discover some new prime numbers! (Hint: Prime numbers always end with
+an odd number that isn’t 5.)
---
+++ project5-FactorFinder/factorfinder.py
@@ -0,0 +1,20 @@
+def factor_finder(number):
+    #loop through all numbers from 1 to the number itself and append the number to the 'factors' list if the inputed number is completely divisible by the number in the current loop 
+    factors = [str(x) for x in range(1,number+1) if number%x==0]
+    return factors
+
+
+if __name__ == "__main__":
+    print("_____________________FACTOR FINDER____________________")
+    print("by Jules Okoye-ezeh")
+    print("This program takes an interger input and returns all the factors of that number. ")
+    print()
+    while True:
+        try:
+            num = int(input("Enter Number: "))
+            break
+        except ValueError:
+            print("\nPlease enter a valid positive integer\n")
+    results = factor_finder(num)
+    print(results)
+    print( f"The factors of {num} are... "+', '.join(results))
